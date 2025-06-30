bef89cc04041b7f24637bc1ef4ba60395399d373
lucasrobaina1 <62635385+lucasrobaina1@users.noreply.github.com>
2025-06-30 18:23:44 -0400

    Updating comments

--- app/logger.py
+++ app/logger.py
@@ -1,10 +1,8 @@
-# app/logger.py
 import logging
 import sys
 from pythonjsonlogger import jsonlogger
 
 def setup_logging():
-    # Crear un logger
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
@@ -12,12 +10,10 @@ def setup_logging():
     if logger.hasHandlers():
         logger.handlers.clear()
 
-    # Crear un handler para la consola y establecer el formato JSON
     log_handler = logging.StreamHandler(sys.stdout)
     formatter = jsonlogger.JsonFormatter(
         '%(asctime)s %(name)s %(levelname)s %(message)s'
     )
     log_handler.setFormatter(formatter)
 
-    # Añadir el handler al logger
     logger.addHandler(log_handler)
